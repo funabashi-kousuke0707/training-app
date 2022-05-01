abb044bf48f3341e6bf90e13d971a99372001499
FunabashiKousuke <funabashi.kousuke.0707@gmail.com>
2022-05-01 09:47:35 +0900

    commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:name,:text).merge(post_id: @post.id)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to root_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:name,:text)
+    end
 end
